c1925def8ab31b6de5bf115517058fb62042b4fd
vlada-marchenko <vvvvadya44@gmail.com>
2026-05-21 22:52:40 +0200

    fix: tokens at middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 const privateRoutes = ["/diary", "/profile", "/journey"];
 const publicRoutes = ["/auth/login", "/auth/register"];
 
 export async function middleware(request: NextRequest) {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
-  console.log("accessToken:", accessToken);
-  console.log("refreshToken:", refreshToken);
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+  const cookieHeader = `accessToken=${accessToken ?? ""}; refreshToken=${
+    refreshToken ?? ""
+  }`;
   const { pathname } = request.nextUrl;
 
   const isPrivateRoute = privateRoutes.some((route) =>
@@ -33,7 +32,7 @@ export async function middleware(request: NextRequest) {
         const refreshUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/refresh`;
         const apiRes = await fetch(refreshUrl, {
           method: "POST",
-          headers: { Cookie: cookieStore.toString() },
+          headers: { Cookie: cookieHeader },
         });
 
         if (apiRes.ok) {
